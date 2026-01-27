fd4da6991985f254e347a2927170a563fdb8730c
sdevfr <kevinyviquel01@hotmail.com>
2026-01-27 15:45:57 +0100

    chore: Bump version to 1.3.21

--- bin/vako.js
+++ bin/vako.js
@@ -21,7 +21,7 @@ const program = new Command();
 program
   .name('vako')
   .description('Vako Framework CLI')
-  .version('1.3.20');
+  .version('1.3.21');
 
 // ============= DEV COMMAND =============
 program
